1199470a5df640a04d00aeda264f638c333af55a
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-09-09 10:36:17 -0300

    Sticky navbar and change a to Link

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,13 +3,22 @@ import './Navbar.css';
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-expand-md">
+        <nav class="navbar navbar-expand-md sticky-top">
             <div class="container-fluid">    
                 <a class="navbar-brand text-dark align-self-center" href="#">RecetApp</a>         
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse d-md-flex align-items-center justify-content-between p-2" id="navbarSupportedContent">
+                    <div class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorias
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><Link class="dropdown-item" to="http://localhost:5000/recipes/carne">Con carne</Link></li>
+                            <li><Link class="dropdown-item" to="http://localhost:5000/recipes/veggie">Veggie</Link></li>
+                            <li><Link class="dropdown-item" to="http://localhost:5000/recipes/postres">Postres</Link></li>
+                        </ul>
+                    </div>
                     <form class="d-flex align-items-center mt-2">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button class="btn btn-outline-success" type="submit">Search</button>
