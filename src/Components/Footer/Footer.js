6d9b92b5cde7308c9c289fa8240959f4d54693a4
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-09-08 23:51:00 -0300

    Footer creation

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,15 @@
 import facebook from './Images/facebook.png';
 import instagram from './Images/instagram.png';
 import twitter from './Images/twitter.png';
+import './Footer.css';
 
 function Footer (){
     return (
-        <div className="rrss">
-            <img src={facebook} alt="" className="rounded-circle" />
-            <img src={twitter} alt="" className="rounded-circle ms-2" />
-            <img src={instagram} alt="" className="rounded-circle ms-2" />
-        </div>
+        <footer class="mt-3 container-fluid d-flex justify-content-center">
+            <img src={facebook} alt="" className="rounded-circle align-self-center" />
+            <img src={twitter} alt="" className="rounded-circle ms-2 align-self-center" />
+            <img src={instagram} alt="" className="rounded-circle ms-2 align-self-center" />
+        </footer>
     );
 }
 
