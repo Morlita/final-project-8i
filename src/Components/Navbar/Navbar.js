7faef878d3b73a7bffcc98fdf1129cd41b60426d
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-09-09 00:53:44 -0300

    Footer creation

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,20 +3,20 @@ import './Navbar.css';
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-md">
             <div class="container-fluid">    
-                <a class="navbar-brand text-white" href="#">RecetApp</a>         
+                <a class="navbar-brand text-dark align-self-center" href="#">RecetApp</a>         
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form class="d-flex justify-content-center">
-                        <input class="form-control me-2 align-self-center" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success align-self-center" type="submit">Search</button>
+                <div class="collapse navbar-collapse d-md-flex align-items-center justify-content-between p-2" id="navbarSupportedContent">
+                    <form class="d-flex align-items-center mt-2">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
-                    <div className="d-flex">
+                    <div className="d-flex align-middle text-center mt-2" >
                         <div className="rounded-pill">
-                            <Link className="nav-link" to="signin">Sig In</Link>
+                            <Link className="nav-link" to="signin">Sing In</Link>
                         </div>  
                         <div className="rounded-pill ms-2">
                             <Link className="nav-link" to="login">Log In</Link>
