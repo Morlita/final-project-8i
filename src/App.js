0f06bc99514b993f1fe85bf8da11b06b9be5ede6
santiago <santiagojimenezmontilla@gmail.com>
2021-09-16 17:12:57 -0300

    creacion de signin con ls

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Main from './Components/Main/Main';
+import Signin from './Components/SingIn/SignIn';
 
 
 import {
@@ -19,6 +20,9 @@ function App() {
           <Switch>
             <Route path='/' exact>
               <Main />
+            </Route>
+            <Route path="/signin">
+              <Signin />
             </Route>
           </Switch>
         </div>
