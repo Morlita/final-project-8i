523d885f70a67f2a622a173b81ee1230c83aaaca
Dolores <dolores.avila.david@gmail.com>
2021-09-16 18:18:41 -0300

    solved map undefinded

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -28,33 +28,37 @@ function Recipe() {
     return (
         <div className='container recipe-component py-2'>
             <article className='container'>
-                <div className="row  py-3">
-                    <div className="card border-0">
+                <div className="row  py-3 grid">
+                    <div className="card border-0 item">
                         <img src={foodpic1} className='rounded' alt="" />
-                        <div className="card-img-overlay d-flex align-items-end ms-3">
-                            <h1 className="h3">{title} <span>{likes} likes</span> <span>{time}</span> </h1>
-                        </div>
+                        <h1 className="h3 label">{title} <span className='float-end mx-1'><i class="bi bi-alarm p-1"></i> {likes} likes</span> <span className='float-end'><i class="bi bi-star p-1"></i> {time} </span> </h1>                        
                     </div>
                 </div>
                 <div className="row">
                     <div className="col col-md-4">
                         <h4 className='p-2 ms-2'>Ingredientes</h4>
                         <ul className='py-2'>
-                            {/* {ingredients.map((ingr, index)=> (
+                            {ingredients && ingredients.map((ingr, index)=> (
                                 <li key={index}>{ingr} <span className='float-end'>100 grs.</span> </li>
-                            ))} */}
+                            ))}
                         </ul>
                     </div>
                     <div className="col-md-1 vl d-sm-none d-md-block"></div>
                     <div className="col col-md-7">
                         <h4 className='p-2 ms-2'>Preparación</h4>
                         <ol className='py-2'>
-                            {/* {steps.map((step, index)=> (
+                            {steps && steps.map((step, index)=> (
                                 <li key={index}>{step}</li>
-                            ))}    */}                         
+                            ))}                            
                         </ol>
                     </div>
                 </div>
+            </article>
+            <hr />
+            <article className='container'>
+                <h6> <span></span>Conservación en freezer: <span>{timeFreezer}</span></h6>
+                <h6> <span></span>Conservación en heladera: <span>{timeFridge}</span> </h6>
+                <h6> <span></span>Subido el <span>{createdAt}</span> </h6>
             </article>
             <hr />
             <div className="container">
