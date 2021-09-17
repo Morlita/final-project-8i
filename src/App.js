361b0389f282c4af049b562e4aea116e231e9727
santiago <santiagojimenezmontilla@gmail.com>
2021-09-17 20:08:54 -0300

    card sin signin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Main from './Components/Main/Main';
-import Signin from './Components/SignIn/SingIn';
 
 
 import {
@@ -20,9 +19,6 @@ function App() {
           <Switch>
             <Route path='/' exact>
               <Main />
-            </Route>
-            <Route path="/signin"> 
-              <Signin />
             </Route>
           </Switch>
         </div>
