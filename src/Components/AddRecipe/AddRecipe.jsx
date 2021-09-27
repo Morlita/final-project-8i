679630f41d44bf0b15fbae6b7152e344daa5c6eb
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-09-26 21:11:08 -0300

    Fixing bugs on the select

--- src/Components/AddRecipe/AddRecipe.jsx
+++ src/Components/AddRecipe/AddRecipe.jsx
@@ -4,18 +4,18 @@ import AddInputIngredients from "./AddInputIngredients";
 
 function AddRecipe() {
   const [recipe, setRecipe] = useState({
-    title: '',
-    time: '',
-    timeFreezer: '',
-    timeFridge: '',
-    category: '',
-    img: ''
-});
+    title: "",
+    time: "",
+    timeFreezer: "",
+    timeFridge: "",
+    category: "",
+    img: "",
+    steps: ""
+  });
 
-console.log(recipe)
+  console.log(recipe);
 
-  
-    /*STEPS */
+  /*ADD A NEW STEP */
   const defaultStateSteps = {
     step: "",
   };
@@ -31,12 +31,14 @@ console.log(recipe)
     setRowSteps(copyRowSteps);
   };
 
-  /*INGREDIENTS */
+  /*ADD A NEW INGREDIENT */
   const defaultStateIngredients = {
     ingredient: "",
     cuantity: "",
   };
-  const [rowIngredients, setRowIngredients] = useState([defaultStateIngredients]);
+  const [rowIngredients, setRowIngredients] = useState([
+    defaultStateIngredients,
+  ]);
 
   const handleOnAddIngredients = () => {
     setRowIngredients(rowIngredients.concat(defaultStateIngredients));
@@ -48,66 +50,129 @@ console.log(recipe)
     setRowIngredients(copyRowIngredients);
   };
 
-  /*SET INFORMATION */
+  /*COPY INFORMATION */
   const setRecipes = (event) => {
     setRecipe({ ...recipe, [event.target.name]: event.target.value });
-    console.log(recipe)
+    console.log(recipe);
   };
 
   return (
     <div className="App">
       <h1>Crea tu receta</h1>
-        <div className="d-flex flex-column form-contact align-items-center">
-            <label htmlFor="" className="mb-1">Nombre</label>
-            <input type="text" name="title" className="w-50 mb-3" value={recipe.title} onChange={setRecipes}/>
+      <div className="d-flex flex-column form-contact">
+        <label htmlFor="" className="mb-1">
+          Nombre
+        </label>
+        <input
+          type="text"
+          name="title"
+          className="w-100 w-md-50 mb-3"
+          value={recipe.title}
+          onChange={setRecipes}
+        />
 
-            {rowSteps.map((row, index) => (
-                <AddInputSteps {...row} remove={() => handleRemoveSteps(index)} key={index} />
-            ))}
+        {rowSteps.map((row, index) => (
+          <AddInputSteps
+            {...row}
+            remove={() => handleRemoveSteps(index)}
+            key={index}
+            onChange={setRecipes}
+          />
+        ))}
 
-            <button className="btn btn-primary mt-3 mb-5 align-self-center" onClick={handleOnAddSteps}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                    <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"/>
-                </svg>
-            </button>
+        <button
+          className="btn btn-primary mt-3 mb-5 w-100 w-md-50"
+          onClick={handleOnAddSteps}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-plus-lg"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+          </svg>
+        </button>
 
-            {rowIngredients.map((row, index) => (
-                <AddInputIngredients {...row} remove={() => handleRemoveIngredients(index)} key={index} />
-            ))}
+        {rowIngredients.map((row, index) => (
+          <AddInputIngredients
+            {...row}
+            remove={() => handleRemoveIngredients(index)}
+            key={index}
+          />
+        ))}
 
-            <button className="btn btn-primary mt-3 mb-5 align-self-center" onClick={handleOnAddIngredients}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                    <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z"/>
-                </svg>
-            </button>
+        <button
+          className="btn btn-primary mt-3 mb-5 w-100 w-md-50"
+          onClick={handleOnAddIngredients}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-plus-lg"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+          </svg>
+        </button>
 
+        <label htmlFor="" className="mb-1">
+          Tiempo Estimado de Preparacion
+        </label>
+        <input
+          type="number"
+          name="time"
+          className="w-100 w-md-50 mb-3"
+          value={recipe.time}
+          onChange={setRecipes}
+        />
 
-            <label htmlFor=""  className="mb-1">Tiempo Estimado de Preparacion</label>
-            <input type="number" name="time" className="w-50 mb-3" value={recipe.time} onChange={setRecipes}/>
+        <label htmlFor="" className="mb-1">
+          Tiempo Estimado de Duracion en el Freezer
+        </label>
+        <input
+          type="number"
+          name="timeFreezer"
+          className="w-100 w-md-50 mb-3"
+          value={recipe.timeFreezer}
+          onChange={setRecipes}
+        />
 
-            <label htmlFor=""  className="mb-1">Tiempo Estimado de Duracion en el Freezer</label>
-            <input type="number" name="timeFreezer" className="w-50 mb-3" value={recipe.timeFreezer} onChange={setRecipes}/>
+        <label htmlFor="" className="mb-1">
+          Tiempo Estimado de Duracion en la Heladera
+        </label>
+        <input
+          type="number"
+          name="timeFridge"
+          className="w-100 w-md-50 mb-3"
+          value={recipe.timeFridge}
+          onChange={setRecipes}
+        />
 
-            <label htmlFor=""  className="mb-1">Tiempo Estimado de Duracion en la Heladera</label>
-            <input type="number" name="timeFridge" className="w-50 mb-3" value={recipe.timeFridge} onChange={setRecipes}/>
+        <label htmlFor="" className="mb-1">
+          Imagen
+        </label>
+        <input
+          type="img"
+          name="img"
+          className="w-100 w-md-50 mb-3"
+          value={recipe.img}
+          onChange={setRecipes}
+        />
 
-            <label htmlFor="" className="mb-1">Imagen</label>
-            <input type="img" name="img" className="w-50 mb-3" value={recipe.img} onChange={setRecipes}/>
+        <label htmlFor="">Categoria</label>
+        <select className="mb-5 w-100 w-md-50" name="category" value={recipe.category} onChange={setRecipes}>
+          <option value="Con carne">Con carne</option>
+          <option value="Veggie">Veggie</option>
+          <option value="Dulces">Dulces</option>
+        </select>
 
-            <label htmlFor="">Categoria</label>
-            <select className="mb-5">
-                <option value="Con carne">Con carne</option>
-                <option value="Veggie">Veggie</option>
-                <option value="Dulces">Dulces</option>
-            </select>
-
-            
-
-            <a className="btn btn-primary w-25 mt-3 mb-5">Agregar</a>
-        </div>
-
-      
-
+        <a className="btn btn-primary w-25 mt-3 mb-5">Agregar</a>
+      </div>
     </div>
   );
 }
