847d6e181e56537f70f49f5617ec7b22a4fbe2d3
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-09-28 18:28:13 -0300

    First skeletom of the 90-search-bar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,7 +1,18 @@
-import React, {useState} from 'react'
+import { useParams } from 'react-router-dom';
+import MediumCard from "../MediumCard/MediumCard";
 
-function SearchBar() {
-    const [data, setData] = useState({});
+function SearchBar(props) {
+    let { searchURL } = useParams();
+    
+  return (
+    <div className='container'>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 my-2">
+            {props.recipe && props.recipe.map(({ _id, title, time, img, likes }) => (
+                <MediumCard id={_id} title={title} time={time} img={img} likes={likes} />
+            ))}
+        </div>
+    </div>
+  )
 }
 
-export default Se
+export default SearchBar;
