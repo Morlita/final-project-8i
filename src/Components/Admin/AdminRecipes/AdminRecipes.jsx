e2fff2f70598a5e98c08d4c716fe5a02ddf15084
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-09-29 15:56:06 -0300

    Add Filter, delete sort

--- src/Components/Admin/AdminRecipes/AdminRecipes.jsx
+++ src/Components/Admin/AdminRecipes/AdminRecipes.jsx
@@ -1,26 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Modal from "../../Modal/Modal";
-import ButtonAccept from "../ButtonAccept/ButtonAccept";
-import ButtonReject from "../ButtonReject/ButtonReject";
+import AdminRecipesStatus from "./AdminRecipesStatus";
 
 function Admin() {
   const [recipes, setRecipes] = useState([]);
-  //const [users, setUsers] = useState([]);
   const [fetchFlag, setFetchFlag] = useState("");
   let [reference, setReference] = useState("");
 
-  recipes &&
-    recipes.sort((a, b) => {
-      if (a.accepted > b.accepted) {
-        return 1;
-      } else if (a.accepted < b.accepted) {
-        return -1;
-      }
-
-      return 0;
-    });
-
-    console.log("SORT", recipes)
+  /* List all recipes */
 
   const getRecipes = async () => {
     await fetch("https://polar-reaches-30197.herokuapp.com/recipes")
@@ -32,19 +19,27 @@ function Admin() {
       });
   };
 
-  /* List all recipes */
   useEffect(() => {
     getRecipes();
   }, [fetchFlag]);
 
   console.log(recipes);
 
-  /*SETEAR EL INDEX*/
+  /*Set Index*/
 
-  const setIndice = (index) => {
+  const setIndex = (index) => {
     reference = recipes[index];
     setReference(reference);
   };
+
+  /*Filter Recipes*/
+    const accept = recipes.filter(p => p.accepted == "accepted");
+    const reject = recipes.filter(p => p.accepted == "rejected");
+    const pendant = recipes.filter(p => p.accepted == "pending");
+
+  console.log("Accepted", accept);
+  console.log('Pending', pendant)
+  console.log('Rejected', reject)
 
   /*Delete*/
   const remove = (index) => {
@@ -77,69 +72,17 @@ function Admin() {
           </tr>
         </thead>
         <tbody>
-          {recipes.map((item, index) => (
-            <tr key={index}>
-              <td className="col-3">{item.title}</td>
-              <td className="col col-sm-6 col-md-5 col-lg-3">
-                <ol>
-                  {item.steps.map((item, index) => (
-                    <li key={index}>{item}</li>
-                  ))}
-                </ol>
-              </td>
-              <td className="col col-sm-2">{item.accepted}</td>
-              <td className="col-1 col-lg-3">
-                {recipes[index].accepted == "pending" ? (
-                  <div className="d-flex">
-                    <ButtonAccept recipe={item} setFetchFlag={setFetchFlag} />
-                    <ButtonReject recipe={item} setFetchFlag={setFetchFlag} />
-                  </div>
-                ) : recipes[index].accepted == "rejected" ? (
-                  <ButtonAccept recipe={item} setFetchFlag={setFetchFlag} />
-                ) : (
-                  <ButtonReject recipe={item} setFetchFlag={setFetchFlag} />
-                )}
-
-                <button
-                  type="button"
-                  className="btn btn-primary w-20 me-3 mb-1"
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  onClick={() => setIndice(index)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-plus-lg"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
-                  </svg>
-                </button>
-
-                <button
-                  className="btn btn-danger w-20 mb-1"
-                  onClick={() => remove(index)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-trash"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                    />
-                  </svg>
-                </button>
-              </td>
-            </tr>
+          <h5>Recetas Pendientes</h5>
+          {pendant && pendant.map((item, index) => (
+            <AdminRecipesStatus id="pendant" title="pendant" recipe={item} index={index} setFetchFlag={setFetchFlag} remove={remove} setIndex={setIndex}/>
+          ))}
+          <h5>Recetas Aceptadas</h5>
+          {accept && accept.map((item, index) => (
+            <AdminRecipesStatus id="accept" recipe={item} index={index} setFetchFlag={setFetchFlag} remove={remove} setIndex={setIndex}/>
+          ))}
+          <h5>Recetas Rechazadas</h5>
+          {reject && reject.map((item, index) => (
+            <AdminRecipesStatus id="reject" recipe={item} index={index} setFetchFlag={setFetchFlag} remove={remove} setIndex={setIndex}/>
           ))}
         </tbody>
       </table>
