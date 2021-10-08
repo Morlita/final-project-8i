c2420af3bcd9eb03a7df0cd1d5bca929e10db498
Florencia Pistan Esparza <flor.pistan.esparza@gmail.com>
2021-10-08 13:51:17 -0300

    updating footer responsiveness

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,6 +6,7 @@ import './Footer.css';
 
 function Footer() {
     return (
+<<<<<<< HEAD
         <footer className="mt-3 pt-3 social-footer container  text-uppercase">
             <div className="row">
                 <div className="d-flex justify-content-center">
@@ -13,23 +14,65 @@ function Footer() {
                     <i className="h1 m-1 p-3 bi bi-twitter"></i>
                     <i className="h1 m-1 p-3 bi bi-instagram"></i>
                     <i className="h1 m-1 p-3 bi bi-whatsapp"></i>
+=======
+        <footer className='mt-3 pt-3 social-footer container text-uppercase'>
+            <div className='row'>
+                <div className='d-flex justify-content-center'>
+                    <i class='h1 m-1 p-3 bi bi-facebook'></i>
+                    <i class='h1 m-1 p-3 bi bi-twitter'></i>
+                    <i class='h1 m-1 p-3 bi bi-instagram'></i>
+                    <i class='h1 m-1 p-3 bi bi-whatsapp'></i>
+>>>>>>> a6ea950 (updating footer responsiveness)
                 </div>
-
             </div>
+<<<<<<< HEAD
             <div className="row">
                 <div className="d-flex justify-content-center h5">                    
                         <Link to='/' className='mx-2 text-decoration-none text-white'>Home</Link>
                         <Link to='#categories' className='mx-2 text-decoration-none text-white'>Categorías</Link>
                         <Link to='' className='mx-2 text-decoration-none text-white'>Filtros</Link>                    
                         <Link to='' className='mx-2 text-decoration-none text-white'>Destacadas</Link>
+=======
+            <div className='container d-flex justify-content-center text-center h5'>
+                <div className='row'>
+                    <div className='col-sm-3'>
+                        <Link to='/' className='mx-2 text-decoration-none text-white'>
+                            Home
+                        </Link>
+                    </div>
+                    <div className='col-sm-3'>
+                        <Link to='' className='mx-2 text-decoration-none text-white'>
+                            Categorías
+                        </Link>
+                    </div>
+                    <div className='col-sm-3'>
+                        <Link to='' className='mx-2 text-decoration-none text-white'>
+                            Filtros
+                        </Link>
+                    </div>
+                    <div className='col-sm-3'>
+                        <Link to='' className='mx-2 text-decoration-none text-white'>
+                            Destacadas
+                        </Link>
+                    </div>
+>>>>>>> a6ea950 (updating footer responsiveness)
                 </div>
             </div>
-            <div className="row mt-2">
-                <div className="d-flex justify-content-center">                    
-                        <Link to='' className='mx-2 text-decoration-none text-white  text-muted'>Quienes somos</Link>
-                        <Link to='' className='mx-2 text-decoration-none text-white  text-muted'>Nuestra misión</Link>                    
+            <div className='row mt-2'>
+                <div className='d-flex justify-content-center'>
+                    <Link
+                        to=''
+                        className='mx-2 text-decoration-none text-white  text-muted'
+                    >
+                        Quienes somos
+                    </Link>
+                    <Link
+                        to=''
+                        className='mx-2 text-decoration-none text-white  text-muted'
+                    >
+                        Nuestra misión
+                    </Link>
                 </div>
-
             </div>
             {/* <div className="row end"></div> */}
         </footer>
