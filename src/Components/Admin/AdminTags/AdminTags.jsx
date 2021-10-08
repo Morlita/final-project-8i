6bd1f6f5d18a1eebd6bc652bf223cc55dc58f5d2
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-10-07 21:09:03 -0300

    Delete the edit button

--- src/Components/Admin/AdminTags/AdminTags.jsx
+++ src/Components/Admin/AdminTags/AdminTags.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 function AdminTags() {
   const [tags, setTags] = useState([]);
   const [fetchFlag, setFetchFlag] = useState("");
-  let [reference, setReference] = useState("");
   const [tag, setTag] = useState({
     name: "",
   });
@@ -66,10 +65,6 @@ function AdminTags() {
     getTags();
   }, [fetchFlag]);
 
-  const setIndex = (index) => {
-    setReference(index);
-  };
-
   return (
     <div className="container">
       <h2>Tags</h2>
@@ -103,24 +98,6 @@ function AdminTags() {
             <tr key={index}>
               <td>{item.name}</td>
               <td>
-                <button
-                  className="btn btn-primary w-20 mb-1 mx-3"
-                  type="button"
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  onClick={() => setIndex(index)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-pen-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z" />
-                  </svg>
-                </button>
                 <button
                   className="btn btn-danger w-20 mb-1"
                   onClick={() => remove(index)}
