e06004b9cf19c9075f4ad33df15d98abf5408927
Dolores <dolores.avila.david@gmail.com>
2021-10-08 11:49:50 -0300

    styled carousel rows

--- src/Components/Tags/Tags.js
+++ src/Components/Tags/Tags.js
@@ -6,31 +6,29 @@ function Tags() {
 
     return (
         <div className="d-flex justify-content-sm-center justify-content-md-start text-uppercase tag-acordeon w-75 rounded">
-            <div className="container pb-5">
-                <h4 className="text-center p-2">También podes usar nuestros filtros...</h4>
-
+            <div className="container pb-3">
+                <h4 className="text-center p-2 pt-3">También podes usar nuestros filtros...</h4>
                 <div class="accordion" id="accordionExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
                             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                <i className="bi bi-funnel h5 p-1"></i> Seleccioná hasta tres opciones:
+                                <i className="bi bi-funnel h4 p-1 mb-1"></i> Seleccioná hasta tres opciones:
                             </button>
                         </h2>
                         <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div class="accordion-body d-flex justify-content-between flex-wrap ">
+                            <div class="accordion-body d-flex justify-content-evenly flex-wrap ">
                                 {tags.map((item, index) => (
                                     <div key={index} class="form-check form-check-inline py-1">
                                         <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off" />
-                                        <label class ="btn btn-outline-secondary" for="btn-check-outlined">{item}</label>
+                                        <label class="btn btn-outline-secondary rounded-pill" for="btn-check-outlined">{item}</label>
                                     </div>
                                 ))}
                             </div>
                         </div>
                     </div>
                 </div>
-
-                <div>
-
+                <div className="filter-button text-center pt-3 d-grid col-3 mx-auto">
+                    <button type="button" class="btn btn-danger rounded-pill">Filtrar</button>
                 </div>
             </div>
         </div>
