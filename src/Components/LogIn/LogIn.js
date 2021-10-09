a9ca0b1445e51751bd0d23f2626f147490332060
Dolores <dolores.avila.david@gmail.com>
2021-10-08 23:26:42 -0300

    starting

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,19 +1,21 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
+import './Login.css'
 
-function Login (){
+function Login() {
 
     let history = useHistory()
 
-    const [usuario,setUsuario] = useState({
-        email:"",
-        password:""
+    const [usuario, setUsuario] = useState({
+        email: "",
+        password: ""
     });
 
-    const setUserObj = (event) =>{
-        setUsuario({...usuario, [event.target.name]:event.target.value});
+    const setUserObj = (event) => {
+        setUsuario({ ...usuario, [event.target.name]: event.target.value });
     }
 
+<<<<<<< HEAD
     const Guardar = ()=>{
 
             const data = JSON.stringify({
@@ -46,29 +48,62 @@ function Login (){
                 
                 
                         
+=======
+    const Guardar = () => {
+
+        const data = JSON.stringify({
+            email: usuario.email,
+            password: usuario.password
+        })
+
+        fetch('https://polar-reaches-30197.herokuapp.com/user/login', {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            method: "POST",
+            body: data
+        }).then(response => response.json()
+        ).then(data => {
+            console.log(data);
+            if (!data.token) {
+                alert("email o contraseña incorrectos");
+            }
+            else {
+                localStorage.setItem("logedUser", JSON.stringify(usuario.email))
+                localStorage.setItem("registerLogIn", JSON.stringify(data))
+                alert("usuario logeado con exito")
+                history.push("/")
+            }
+
+        })
+
+
+
+>>>>>>> fb6c53b (starting)
     }
 
     return (
-        <div>
-            <div className="container">
-                <h1 className="text-center">RecetApp</h1>
-                <br/>
-                <h3 className="text-center">Ingresar</h3>
-                <form id ="formLogIn">
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" onChange={setUserObj}/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" name="password" onChange={setUserObj}/>
-                    </div>
-                    <div>
+
+        <div className="container">
+            <h1 className="text-center">RecetApp</h1>
+            <br />
+            <h3 className="text-center">Ingresar</h3>
+            <form id="formLogIn">
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" onChange={setUserObj} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" name="password" onChange={setUserObj} />
+                </div>
+                <div>
                     <button type="button" className="btn btn-primary m-3" onClick={Guardar}>Ingresar</button>
-                    </div>
-                </form>
-            </div>
+                </div>
+            </form>
         </div>
+
     );
 }
 
--- src/Components/SingIn/SignIn.js
+++ src/Components/SingIn/SignIn.js
@@ -1,23 +1,24 @@
-import React ,{useState} from "react";
-import {useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory, Link } from 'react-router-dom';
 import emailjs from 'emailjs-com';
+import './SignIn.css'
 
-function Signin (){
+function Signin() {
 
     const emailRegexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     const LettersRegexp = /[a-z]/g
 
     let history = useHistory()
 
-    const [usuario,setUsuario] = useState({
+    const [usuario, setUsuario] = useState({
         name: "",
         lastName: "",
-        email:"",
-        password:""
+        email: "",
+        password: ""
     });
 
-    const setUserObj = (event) =>{
-        setUsuario({...usuario, [event.target.name]:event.target.value});
+    const setUserObj = (event) => {
+        setUsuario({ ...usuario, [event.target.name]: event.target.value });
     }
 
     let parametros = {
@@ -25,86 +26,89 @@ function Signin (){
         email: usuario.email
     }
 
-    let sendEmail = ()=>{
+    let sendEmail = () => {
         emailjs.send('gmail', 'template_e0meufb', parametros, "user_CjWnZrzaDNFhrMVuulGpr")
-    .then(function(response) {
-       alert("registro exitoso, revisa tu casilla de correo")
-    });
+            .then(function (response) {
+                alert("registro exitoso, revisa tu casilla de correo")
+            });
     }
 
-    const Guardar = ()=>{
+    const Guardar = () => {
 
-        if(usuario.name.length < 2 || usuario.name.length > 25 || LettersRegexp.test(usuario.name) === false ){
+        if (usuario.name.length < 2 || usuario.name.length > 25 || LettersRegexp.test(usuario.name) === false) {
             alert("Nombre invalido")
         }
-        else if(usuario.lastName.length < 3 || usuario.lastName.length > 25 || LettersRegexp.test(usuario.lastName) === false ){
+        else if (usuario.lastName.length < 3 || usuario.lastName.length > 25 || LettersRegexp.test(usuario.lastName) === false) {
             alert("appellido invalido")
         }
-        else if(usuario.email === "" || emailRegexp.test(usuario.email) === false){
+        else if (usuario.email === "" || emailRegexp.test(usuario.email) === false) {
             alert("email invalido")
         }
-        else if(usuario.password.length < 6){
+        else if (usuario.password.length < 6) {
             alert("contraseña invalida")
         }
-        else{
-            
+        else {
+
             const data = JSON.stringify({
                 email: usuario.email,
                 password: usuario.password,
                 name: usuario.name,
                 lastName: usuario.lastName,
-                role:"user"
+                role: "user"
             })
 
-              fetch('https://polar-reaches-30197.herokuapp.com/user/', {
-                  headers:{
-                      "Accept": "application/json",
-                      "Content-Type": "application/json"
-                  },
-                  method:"POST",
-                  body: data
-              }).then(response => {
-                  if(response.status === 200){
+            fetch('https://polar-reaches-30197.herokuapp.com/user/', {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                body: data
+            }).then(response => {
+                if (response.status === 200) {
                     sendEmail();
                     history.push("/")
-                  }
-                  else{
-                      alert("ocurrio un error")
-                  }
-              })      
+                }
+                else {
+                    alert("ocurrio un error")
+                }
+            })
         }
-        
+
     }
 
     return (
-        <div>
-            <div className="container">
-                <h1 className="text-center">RecetApp</h1>
-                <br/>
-                <h3 className="text-center">Registrate</h3>
-                <form id ="formUsuario">
-                    <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Nombre</label>
-                        <input type="text" className="form-control" id="name" name="name" aria-describedby="Username" onChange={setUserObj} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="lastName" className="form-label">Apellido</label>
-                        <input type="text" className="form-control" id="lastName" name="lastName" aria-describedby="Username" onChange={setUserObj}/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" onChange={setUserObj}/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" name="password" onChange={setUserObj}/>
-                    </div>
-                    <div>
-                    <button type="button" className="btn btn-primary m-3" onClick={Guardar}>Registrarme</button>
-                    </div>
-                </form>
-            </div>
+
+        <div className="container signin-component rounded mt-4 px-4 py-2 d-grid col-md-7 col-10">
+            <h3 className="text-center pt-2">Primera vez en RecetApp?</h3>
+            <h1 className="text-center py-2 h2">Ingresá tus datos para comenzar</h1>
+            <form id="formUsuario">
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Nombre</label>
+                    <input type="text" className="form-control" id="name" name="name" aria-describedby="Username" onChange={setUserObj} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="lastName" className="form-label">Apellido</label>
+                    <input type="text" className="form-control" id="lastName" name="lastName" aria-describedby="Username" onChange={setUserObj} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" onChange={setUserObj} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" name="password" onChange={setUserObj} />
+                </div>
+                <div className="d-grid col-5 mx-auto register-button">
+                    <button type="button" className="btn btn-danger m-3 rounded-pill" onClick={Guardar}>Registrarme</button>
+                </div>
+                <div className="text-center pb-2">
+                    <p className="text-muted mb-0">O si ya estás registrado:</p>
+                    <Link to="/login" className="text-decoration-none or-sign-in">Iniciar sesión</Link> 
+                </div>
+            </form>
         </div>
+
 
     );
 }
