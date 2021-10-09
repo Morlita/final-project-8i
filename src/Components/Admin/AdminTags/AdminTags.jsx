a8373dc4f0de59c1462301721cb90c7bfb69e58a
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-10-09 11:12:26 -0300

    Changing from class to className and responsiveness of adminRecipe

--- src/Components/Admin/AdminTags/AdminTags.jsx
+++ src/Components/Admin/AdminTags/AdminTags.jsx
@@ -30,7 +30,7 @@ function AdminTags() {
 
   /*Add Tags */
   const add = () => {
-    if (tag.name == ""){
+    if (tag.name === ""){
         alert("Complete todos los campos");
     }else{
       fetch('https://polar-reaches-30197.herokuapp.com/tags', {
@@ -79,7 +79,7 @@ function AdminTags() {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-plus-lg"
+            className="bi bi-plus-lg"
             viewBox="0 0 16 16"
           >
             <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
@@ -112,7 +112,7 @@ function AdminTags() {
                   >
                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                     <path
-                      fill-rule="evenodd"
+                      fill-Rule="evenodd"
                       d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
                     />
                   </svg>
