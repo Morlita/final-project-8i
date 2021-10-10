c89f821d642a024175ef105550db28aa91065602
Eugenia Arias Fagalde <eugeniaariasfagalde22@gmail.com>
2021-10-10 18:53:00 -0300

    Tomando el token del localStorage

--- src/Components/AddComment/AddComment.jsx
+++ src/Components/AddComment/AddComment.jsx
@@ -8,7 +8,7 @@ function AddComment({recipeId, reloadFlag, setReloadFlag}) {
 
     const [data, setData] = useState(null);
     const [comments, setComments] = useState([]);
-    const tokenUser = JSON.parse(localStorage.getItem("userToken"));
+    const userToken = JSON.parse(localStorage.getItem("userToken"));
     const user = JSON.parse(localStorage.getItem("registerLogIn"));
 
     const getData = (val) => {
@@ -43,7 +43,7 @@ function AddComment({recipeId, reloadFlag, setReloadFlag}) {
             headers: {
               'Accept': 'application/json',
               'Content-Type': 'application/json',
-              'x-access-token' : tokenUser
+              'x-access-token' : userToken
             },
             body: JSON.stringify({
                 content: data,
@@ -54,7 +54,19 @@ function AddComment({recipeId, reloadFlag, setReloadFlag}) {
         .then((response) => setReloadFlag(response.json()))
         }
     }
-    // POST data to endpoint
+    
+    const deleteComment = (index) => {
+        fetch(`https://polar-reaches-30197.herokuapp.com/comments/${comments[index]._id}`,
+            {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+                "x-access-token": userToken,
+              },
+              body: JSON.stringify(),
+            }
+          ).then((response) => setReloadFlag(response.json()));
+    }
 
     useEffect(() => {
         getComments();
@@ -81,7 +93,15 @@ function AddComment({recipeId, reloadFlag, setReloadFlag}) {
             <div>
                 <h4 className="mt-3">Comentarios:</h4>
                 {comments.length === 0 ? <div id="floatingAddComment" className="form-text">Esta receta aun no tiene comentarios</div>: comments.map(({userName, userLastName, content}, index) => (
-                    <p key={index}><span className="fw-bold">{userName} {userLastName}</span> {content}</p>
+                    <div>
+                        <p key={index}><span className="fw-bold">{userName} {userLastName}</span> {content}
+                        <button className="float-end" type="button" onClick={() => deleteComment(index)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                            </svg>
+                        </button></p>
+                    </div>
                 ))}
             </div>
         </div>
