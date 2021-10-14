38cfd027aff63d66b0da804ac0fbcdd88617accb
Dolores <dolores.avila.david@gmail.com>
2021-10-13 21:25:47 -0300

    style

--- src/Components/ProfileCard/ProfileCard.js
+++ src/Components/ProfileCard/ProfileCard.js
@@ -1,17 +1,20 @@
+import '../UserProfile/UserProfile.css'
+import Chef from '../Img/chef.png'
+
 function ProfileCard(props){
 
     return(
-        <div className="card mb-3">
+        <div className="card mb-3 profile-card p-3 w-75 mx-auto">
             <div className="row g-0">
-                <div className="col-md-4">
-                <img src="https://trello.com/1/cards/6132b0dce9417a02fc485e4f/attachments/614ba89423e8142e46e0b335/download/Captura_de_pantalla_2021-09-22_190405.jpg" className="img-fluid rounded-start" alt="..." style={{height: "250px"}}/>
+                <div className="col-md-4 d-flex align-items-center justify-content-center">
+                    <img src={Chef} className="img-fluid rounded " alt="avatar"/>
                 </div>
                 <div className="col-md-8">
                 <div className="card-body">
-                    <h3 className="card-title">{props.name} {props.lastName}</h3>
-                    <p className="card-text ">recetas ({props.numeroRecetas})</p>
-                    <p className="card-text ">favoritos ({props.numeroFav})</p>
-                    <p className="card-text ">notas ({props.notas})</p>
+                    <h2 className="card-title h1">{props.name} {props.lastName}</h2>
+                    <p className="card-text ">Mis recetas ({props.numeroRecetas})</p>
+                    <p className="card-text ">Mis favoritos ({props.numeroFav})</p>
+                    <p className="card-text ">Mis notas ({props.notas})</p>
                 </div>
                 </div>
             </div>
