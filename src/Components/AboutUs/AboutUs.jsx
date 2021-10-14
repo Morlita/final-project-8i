fb0024def7445ac5811f67a1f88a0f887445a5f0
santiago <santiagojimenezmontilla@gmail.com>
2021-10-14 16:39:07 -0300

    ahora si

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -11,7 +11,7 @@ function AboutUs() {
                 <p className="fw-bold">Si sos un apasionado de la cocina o solo estas buscando nuevos sabores, en Recetapp vas a encontrar la receta ideal para vos.</p>
                 <p className="fw-bold">Entradas, aperitivos, ensaladas, pastas, carnes, resposteria, Recetapp sorprende con una gran variedad de recetas de cocina fáciles a la vez que gustosas, que te dejarán sin palabras.</p>
                 <p className="fw-bold">Contamos con un sistema de categorias y tags para que tu busqueda sea lo mas rapida y efectiva posible.</p>
-                <p className="fw-bold">Nuestra web crece día a día gracias a nuestros usuarios que desean compartir sus mejores recetas.</p>
+                <p className="fw-bold">Nuestra web crece día a día gracias a nuestros usuarios que desean compartir sus mejores recetas!.</p>
             </h4>
         </div>
     )
