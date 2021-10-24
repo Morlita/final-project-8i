a83ac522dc4c1abc6b655639474f5b875aa9e76c
Dolores <dolores.avila.david@gmail.com>
2021-10-24 12:01:55 -0300

    info of all members

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -61,11 +61,11 @@ function AboutUs() {
                         </div>
                         <div class="founder-bcg m-2 p-2 align-self-center">
                             <h3 class="font-weight-bolder">Santiago Calvo</h3>
-                            <h5 class="mb-3">Master en Dirección de Empresas</h5>
+                            <h5 class="mb-3">Desarrolador Full Stack</h5>
+                            <h5 class="mb-3">Músico</h5>
                             <hr />
-                            <p>Fundó LIAS en 1984, junto a la Arquitecta Mara David.</p>
-                            <p>Bajo su dirección, LIAS se constituyó como el único laboratorio certificado del NOA.Aún hoy, se
-                                mantiene líder en el mercado, con más de 700 clientes que lo eligen desde el primer día.</p>
+                            <p>Con 29 años, Santi domina React y la guitarra por igual. Generoso, colaborador y trabajador.</p>
+                            <p>Actualmente trabaja en Rolling Code y otras empresas.</p>
                         </div>
                     </div>
                 </section>
@@ -75,30 +75,25 @@ function AboutUs() {
                             <h3 class="">Dolores Ávila</h3>
                             <h5>Médica Especialista en Anatomía Patológica</h5>
                             <h5 class="mb-3">Wannabe Developer</h5>
-                            <p>Hija</p>
                             <hr />
-                            <p>Nació y estudió en Tucumán.Luego de recibirse de médica, se mudó a Bs As para hacer la
-                                residencia.Cuatro años después, se mudó a EEUU durante un año para hacer un fellowship en
-                                Dermatopatología.Hace dos años está de vuelta en Bs As, y ahora quiere aprender programación.
-                            </p>
+                            <p>Con 35 años, Loli es médica especialista en patología quirúrgica y dermatopatología. Con ganas de cambiar de profesión, empezó a estudiar programación. Repostera aficionada</p>
+                            <p>Actualmente trabaja en el Sanatorio Güemes de Palermo</p>
                         </div>
                         <div class="member-pic d-flex align-items-center">
-                            <img src={Loli} alt="Foto de Dolores Avila" />
+                            <img src={Loli} alt="Foto de Loli" />
                         </div>
                     </div>
                 </section>
                 <section class="founder-info my-5 text-white p-4 background-blue">
                     <div class="container d-flex justify-content-md-between ">
                         <div class="member-pic d-flex align-items-center">
-                            <img src={SantiJM} alt="Foto de Santi Calvo" />
+                            <img src={SantiJM} alt="Foto de Santi JM" />
                         </div>
                         <div class="founder-bcg m-2 p-2 align-self-center">
                             <h3 class="font-weight-bolder">Santiago Jimenez Montilla</h3>
-                            <h5 class="mb-3">Master en Dirección de Empresas</h5>
+                            <h5 class="mb-3">Padre</h5>
                             <hr />
-                            <p>Fundó LIAS en 1984, junto a la Arquitecta Mara David.</p>
-                            <p>Bajo su dirección, LIAS se constituyó como el único laboratorio certificado del NOA.Aún hoy, se
-                                mantiene líder en el mercado, con más de 700 clientes que lo eligen desde el primer día.</p>
+                            <p>Con 21 años, Santi es desarrolador Full stack, pero front-end si le dan a elegir. Trabajador responsable, un bebé recién nacido no le impidió realizar todas sus tareas en el proyecto.</p>
                         </div>
                     </div>
                 </section>
@@ -106,32 +101,29 @@ function AboutUs() {
                     <div class="container  d-flex justify-content-md-between">
                         <div class="developer-bcg m-2 p-2 align-self-center">
                             <h3 class="">Eugenia Arias Fagalde</h3>
-                            <h5>Médica Especialista en Anatomía Patológica</h5>
-                            <h5 class="mb-3">Wannabe Developer</h5>
-                            <p>Hija</p>
+                            <h5>Alumna del Colego NC</h5>
+                            <h5 class="mb-3">Aficionada  a la robótica</h5>
                             <hr />
-                            <p>Nació y estudió en Tucumán.Luego de recibirse de médica, se mudó a Bs As para hacer la
-                                residencia.Cuatro años después, se mudó a EEUU durante un año para hacer un fellowship en
-                                Dermatopatología.Hace dos años está de vuelta en Bs As, y ahora quiere aprender programación.
-                            </p>
+                            <p>Con 14 años, Euge cursa 3° año del colegio. Participa de olimpíadas matemáticas y hace cursos de robótica. Odia las presentaciones orales.</p>
+                            <p>Que su edad no te confunda... fue el pilar fundamental de responsabilidad y ejecución en este proyecto.</p>
                         </div>
                         <div class="member-pic d-flex align-items-center">
-                            <img src={Euge} alt="Foto de Dolores Avila"/>
+                            <img src={Euge} alt="Foto de Euge"/>
                         </div>
                     </div>
                 </section>
                 <section class="founder-info my-5 text-white p-4 background-blue">
                     <div class="container d-flex justify-content-md-between ">
                         <div class="member-pic d-flex align-items-center">
-                            <img src={Gustavo} alt="Foto de Santi Calvo" />
+                            <img src={Gustavo} alt="Foto de Gustavo" />
                         </div>
                         <div class="founder-bcg m-2 p-2 align-self-center">
                             <h3 class="font-weight-bolder">Gustavo Leyria</h3>
-                            <h5 class="mb-3">Master en Dirección de Empresas</h5>
+                            <h5 class="mb-3">Contador</h5>
+                            <h5 class="mb-3">Postgrado en Dirección de Empresas y Análisis Bursátil</h5>
                             <hr />
-                            <p>Fundó LIAS en 1984, junto a la Arquitecta Mara David.</p>
-                            <p>Bajo su dirección, LIAS se constituyó como el único laboratorio certificado del NOA.Aún hoy, se
-                                mantiene líder en el mercado, con más de 700 clientes que lo eligen desde el primer día.</p>
+                            <p>Con 41 años, tiene experiencia en liderar proyectos de gran escala y es consultor de empresas públicas. Sus hobbies incluyen correr y hacer degustaciones gastronómicas.</p>
+                            <p>Tester del equipo, y experto subidor de recetas (pero no las prueben...puede que no tengan las cantidades precisas).</p>
                         </div>
                     </div>
                 </section>
@@ -139,32 +131,29 @@ function AboutUs() {
                     <div class="container  d-flex justify-content-md-between">
                         <div class="developer-bcg m-2 p-2 align-self-center">
                             <h3 class="">Florencia Pistan</h3>
-                            <h5>Médica Especialista en Anatomía Patológica</h5>
-                            <h5 class="mb-3">Wannabe Developer</h5>
-                            <p>Hija</p>
+                            <h5>Desarroladora Full Stack</h5>
+                            <h5 class="mb-3">Tutora</h5>
                             <hr />
-                            <p>Nació y estudió en Tucumán.Luego de recibirse de médica, se mudó a Bs As para hacer la
-                                residencia.Cuatro años después, se mudó a EEUU durante un año para hacer un fellowship en
-                                Dermatopatología.Hace dos años está de vuelta en Bs As, y ahora quiere aprender programación.
-                            </p>
+                            <p>Dejó la agronomía para dedicarse a la tecnología e informática. Gran tutora, siempre disponible y predispuesta, lista para compartirte el video exacto que necesitabas.</p>
+                            <p>10/10 would recommend.</p>
                         </div>
                         <div class="member-pic d-flex align-items-center">
-                            <img src={Flor} alt="Foto de Dolores Avila" />
+                            <img src={Flor} alt="Foto de Flor" />
                         </div>
                     </div>
                 </section>
                 <section class="founder-info my-5 text-white p-4 background-blue">
                     <div class="container d-flex justify-content-md-between ">
                         <div class="member-pic d-flex align-items-center">
-                            <img src={LeDiegue} alt="Foto de Santi Calvo" />
+                            <img src={LeDiegue} alt="Foto de Diego" />
                         </div>
                         <div class="founder-bcg m-2 p-2 align-self-center">
                             <h3 class="font-weight-bolder">Le Diegue</h3>
-                            <h5 class="mb-3">Master en Dirección de Empresas</h5>
+                            <h5 class="mb-3">Desarrolador Full Stack Senior</h5>
+                            <h5 class="mb-3">Mentor</h5>
                             <hr />
-                            <p>Fundó LIAS en 1984, junto a la Arquitecta Mara David.</p>
-                            <p>Bajo su dirección, LIAS se constituyó como el único laboratorio certificado del NOA.Aún hoy, se
-                                mantiene líder en el mercado, con más de 700 clientes que lo eligen desde el primer día.</p>
+                            <p>Con 30 años, LeDiegue tomó las riendas de la comisión 8i y los dirigió al éxito. Mentor predispuesto y atento a las necesidades del grupo.</p>
+                            <p>Aficionado a las motos, y ex adicto a pedidos ya, Diego supo guiar a sus alumnos a la finalización exitosa de este proyecto, llamado <span className='fst-italic fw-bold text-danger fs-5'>RecetApp</span>.</p>
                         </div>
                     </div>
                 </section>
