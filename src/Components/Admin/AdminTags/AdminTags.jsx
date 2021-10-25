a63bb8e4bf78cdbc046b034a5b6bdd284ae53057
Dolores <dolores.avila.david@gmail.com>
2021-10-24 21:23:36 -0300

    responsiveness admin

--- src/Components/Admin/AdminTags/AdminTags.jsx
+++ src/Components/Admin/AdminTags/AdminTags.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import '../Admin.css'
 
 function AdminTags() {
   let history = useHistory();
@@ -80,66 +81,64 @@ function AdminTags() {
 
   return (
     <div className="container">
-      <h2>Tags</h2>
-      <div>
-        <label htmlFor="name" className="mx-3">
-          Nuevo tag
-        </label>
-        <input type="text" name="name" value={tag.name} maxLength="15" placeholder="Ej: Salado" onChange={setTagsObj} />
-        <button
-          className="btn btn-primary w-20 mb-1 mx-3"
+      <h2 className='text-center shadow border-bottom-0 rounded p-2 text-uppercase m-3 tag-title'>Tags</h2>
+      <div class="input-group my-4 p-4 shadow rounded border border-success">
+        <input type="text" className="form-control border border-success" placeholder="Agregar TAG (Ej: 'legumbre')" aria-label="New tag" aria-describedby="button-addon2" value={tag.name} maxLength="15" onChange={setTagsObj} name="name"/>
+        <button 
+          className ="btn btn-success" 
+          type ="button" 
+          id="button-addon2"
           onClick={add}
-          type="button"
+          ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="12"
+          fill="currentColor"
+          className="bi bi-plus-lg"
+          viewBox="0 0 16 16"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-plus-lg"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
-          </svg>
-        </button>
+          <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+        </svg></button>
       </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Tags</th>
-            <th scope="col">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tags &&
-            tags.map((item, index) => (
-              <tr key={index}>
-                <td>{item.name}</td>
-                <td>
-                  <button
-                    className="btn btn-danger w-20 mb-1"
-                    onClick={() => remove(index)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-trash"
-                      viewBox="0 0 16 16"
+      <div className="container p-4 shadow rounded">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Tags</th>
+              <th scope="col" className='text-end'>Eliminar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tags &&
+              tags.map((item, index) => (
+                <tr key={index}>
+                  <td className='text-capitalize'>{item.name}</td>
+                  <td className='text-end'>
+                    <button
+                      className="btn btn-danger w-20 mb-1"
+                      onClick={() => remove(index)}
                     >
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                      <path
-                        fill-Rule="evenodd"
-                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                      />
-                    </svg>
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-trash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                        <path
+                          fill-Rule="evenodd"
+                          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                        />
+                      </svg>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- src/Components/Admin/AdminUsers/AdminUsers.jsx
+++ src/Components/Admin/AdminUsers/AdminUsers.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import ModalUser from "./ModalUser/ModalUser";
+import '../Admin.css'
 
 function AdminUsers() {
   let history = useHistory();
@@ -64,73 +65,63 @@ function AdminUsers() {
 
   return (
     <div className="container">
-      <h2>Usuarios</h2>
+      <h2 className='text-center shadow border-bottom-0 rounded p-2 text-uppercase m-3 tag-title'>Usuarios</h2>
       {/*TABLA */}
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Nombre</th>
-            <th scope="col">Apellido</th>
-            <th scope="col" className="d-none d-md-table-cell">
-              Email
-            </th>
-            <th scope="col">Rol</th>
-            <th scope="col">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((item, index) => (
-            <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.lastName}</td>
-              <td className="d-none d-md-table-cell">{item.email}</td>
-              <td>{item.role}</td>
-              <td>
-                <button
-                  className="btn btn-danger w-20 mb-1"
-                  onClick={() => remove(index)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-trash"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                    <path
-                      fill-Rule="evenodd"
-                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                    />
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary w-20 me-3 mb-1 d-md-none"
-                  data-bs-toggle="modal"
-                  data-bs-target="#userModal"
-                  onClick={() => setIndex(index)}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-chevron-bar-expand"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M3.646 10.146a.5.5 0 0 1 .708 0L8 13.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-4.292a.5.5 0 0 0 .708 0L8 2.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708zM1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8z"
-                    />
-                  </svg>
-                </button>
-              </td>
+      <div className="table-responsive">
+        <table className="table my-4 table-hover ">
+          <thead className='text-uppercase table-light'>
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Apellido</th>
+              <th scope="col" className="d-none d-md-table-cell">
+                Email
+              </th>
+              <th scope="col" className='text-center'>Rol</th>
+              <th scope="col" className='text-md-center'>Acciones</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((item, index) => (
+              <tr key={index}>
+                <td className='text-break text-capitalize'>{item.name}</td>
+                <td className='text-break text-capitalize'>{item.lastName}</td>
+                <td className="d-none d-md-table-cell text-lowercase">{item.email}</td>
+                <td className='text-uppercase text-center'>{item.role}</td>
+                <td className='text-center'>
+                  <button
+                    className="btn btn-danger w-20 mb-1"
+                    onClick={() => remove(index)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                      <path
+                        fill-Rule="evenodd"
+                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                      />
+                    </svg>
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-primary w-20 mb-1 d-md-none"
+                    data-bs-toggle="modal"
+                    data-bs-target="#userModal"
+                    onClick={() => setIndex(index)}
+                  >
+                    <i class="bi bi-info-circle"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <ModalUser user={reference} />
     </div>
   );
